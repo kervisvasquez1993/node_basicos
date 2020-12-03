60a77603e44ec48355466a6a24ea36a7fd2beca2
kervisvasquez1993 <kervisvasquez24@gmail.com>
2020-12-03 18:24:25 -0400

    agregamos eccion de promesa

---
+++ 02_clase_2/promesas.js
@@ -0,0 +1,62 @@
+let empleados = [
+    {id: 1 , nombre : 'kervis'},
+    {id: 2 , nombre : 'coro'},
+    {id: 3 , nombre : 'juan'},
+    {id: 4 , nombre : 'karol'},
+];
+let salarios = [
+    { id: 1, salario : 1000 },
+    { id: 2, salario : 2000 },
+]
+
+let getEmpleado = (id,callback) => {
+    return new Promise( (resolve, reject ) => {
+
+        let empleadoDB = empleados.find(empleado => empleado.id == id )
+        if(!empleadoDB)
+        {
+            reject(`no existe empleado con el ID ${id}`)
+        }
+        else 
+        {
+            resolve(empleadoDB)
+        }
+
+    })
+   
+}
+
+const getSalario = (empledo) => 
+{
+    return new Promise((resolve,reject) =>
+    {
+        let salarioDB = salarios.find(salario =>  salario.id == empledo.id)
+
+        if(!salarioDB)
+        {
+            reject(`no existe salario para ${empledo.nombre}`)
+        }
+        else
+        {
+            resolve({
+                nombre  : empledo.nombre,
+                salario : salarioDB.salario,
+                id : empledo.id
+            })
+        }
+    })
+    
+
+    
+}
+
+getEmpleado(3).then(empleado => {
+    console.log('Empleado de base de datos', empleado)
+    getSalario(empleado).then(resp => {
+        console.log(`El salario de ${resp.nombre} es de ${resp.salario}$`)
+    }, (e) => {
+        console.log(e)
+    })
+}, (err) => {
+    console.log(err)
+})
