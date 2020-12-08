b0784bb1e9498cc0aed7e36fe1d0daf4e7f71923
kervisvasquez1993 <kervisvasquez24@gmail.com>
2020-12-08 16:59:41 -0400

    agregamos comit

--- 03_base/app.js
+++ 03_base/app.js
@@ -1,14 +1,13 @@
 const {crearArchivo} = require('./multiplicar/multpiplicar');
 // const fs = require('express');
 // const fs = require('./fs');
-let base = 7
+//let base = 7
 /* console.log(process.argv) */
 
-/* let argv = process.argv
+let argv = process.argv
 let parametro = argv[2]
-let 
- */
+let base = parametro.split("=")[1]
 
 crearArchivo(base)
 .then(archivo => console.log(`archivo creado ${archivo}`))
-.catch(e => console.log(e))
+.catch(e => console.log(e)) 
