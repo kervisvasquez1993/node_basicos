e891728935724477293b35320834a0ae4306313a
kervisvasquez1993 <kervisvasquez24@gmail.com>
2020-12-10 08:29:10 -0400

    agregamos yargs a nuestra dependencia

--- 03_base/app.js
+++ 03_base/app.js
@@ -8,6 +8,7 @@ let argv = process.argv
 let parametro = argv[2]
 let base = parametro.split("=")[1]
 
+
 crearArchivo(base)
 .then(archivo => console.log(`archivo creado ${archivo}`))
 .catch(e => console.log(e)) 
