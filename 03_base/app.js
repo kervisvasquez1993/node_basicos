7e4368863282f1e958d2fd833cc127983f49b440
kervisvasquez1993 <kervisvasquez24@gmail.com>
2020-12-10 09:46:57 -0400

    agregamos yargs y sus unciones basicas

--- 03_base/app.js
+++ 03_base/app.js
@@ -1,14 +1,32 @@
+const argv = require('yargs').command(
+    'listar','Imprime en consola la table de multiplicar',{
+        base: {
+            demand: true,
+            alias: 'b'
+        },
+        limite :{
+            alias: 'l',
+            default : 10
+        }
+    })
+    .help()
+    .argv
 const {crearArchivo} = require('./multiplicar/multpiplicar');
 // const fs = require('express');
 // const fs = require('./fs');
 //let base = 7
 /* console.log(process.argv) */
 
-let argv = process.argv
-let parametro = argv[2]
-let base = parametro.split("=")[1]
+/* let argv2 = process.argv */
 
+/*  
+console.log('limite', argv.limite)
+//let parametro = argv[2] */
+//let base = parametro.split("=")[1]
 
+/* 
 crearArchivo(base)
 .then(archivo => console.log(`archivo creado ${archivo}`))
-.catch(e => console.log(e)) 
+.catch(e => console.log(e))  */
+
+console.log(argv)
