d1c86631a7ea7072ff14c8458906c0b3d1ada706
kervis vasquez <kervisvasquez24@gmail.com>
2021-07-21 12:01:43 -0400

    gregamos yargs a nuestro respositorio

--- seccion4/app.js
+++ seccion4/app.js
@@ -1,24 +1,24 @@
 // imprimir la tabla del 5 en consola 
 const {crearArchivo} = require('./helpers/multiplicar')
+const argv = require('yargs').argv
 console.clear();
 
 let indice = 1
-    /* base   = 5 */
-const [,, arg3 = 'base=1'] = process.argv;
-const [, base = 1] = arg3.split('=')
-    console.log(
+     base   = 5 
+/*   */
+  /*   console.log(
             `
                 =================================
                        TABLA DEL ${base}
                 =================================
-            `  )
+            `  ) */
 
 /* console.log(process.argv); */
 
 /* console.log(base1); */
 
 
- salida = crearArchivo(indice, base).then(
+/*  salida = crearArchivo(indice, base).then(
     archivo => console.log(archivo)
 )
- 
+  */
