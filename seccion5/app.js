a63e45cf984f1175bca310c4997aa759cc5f0fb5
kervis vasquez <kervisvasquez24@gmail.com>
2021-07-22 16:59:18 -0400

    agregamos tarea a listado de tarea

--- seccion5/app.js
+++ seccion5/app.js
@@ -1,16 +1,25 @@
 require('colors')
 const {iniquirerMenu, pausa} = require('./helpers/inquirer')
+const Tarea = require('./models/tarea')
+const Tareas = require('./models/tareas')
 const main = async () => {
     console.clear()
     let opt = ''
     do
     {
-        opt = await iniquirerMenu();
+       /*  opt = await iniquirerMenu();
         console.log({opt});
        if(opt !== '0' ) 
         {
             await pausa()   
-        } 
+        }  */
+        const tarea = new Tarea('hacer ejercicio')
+        const tareas = new Tareas()
+        
+        tareas._listado[tarea.id] = tarea;
+        console.log(tareas)
+        await pausa()
+
     }
     while(opt !=='0')
     {
