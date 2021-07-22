e85cf070ae39a4e0e3fce9eb9600491544aa635f
kervis vasquez <kervisvasquez24@gmail.com>
2021-07-22 11:00:21 -0400

    📦️ add package inquirer

---
+++ seccion5/app.js
@@ -0,0 +1,22 @@
+require('colors')
+const {mostrarMenu, pausa} = require('./helpers/msj')
+const main = async () => {
+    console.log('hola mundo')
+    let opt = ''
+    do
+    {
+        opt = await mostrarMenu();
+        console.log({opt});
+        if(opt !== '0' ) 
+        {
+            await pausa()   
+        }
+    }
+    while(opt !=='0')
+    {
+
+    }
+
+    
+}
+main()
---
+++ seccion5/helpers/msj.js
@@ -0,0 +1,54 @@
+require('colors');
+require('inquirer');
+
+const mostrarMenu = () =>
+{
+
+    return new Promise(resolve => 
+        {
+            console.log(`=======================`.green)
+            console.log('Seleccione una ópcion'.red)
+            console.log('=======================\n'.green)
+            console.log(`${'1'.green}. Crear Una Tarea
+                         ${'2'.green}. Listar Tareas
+                         ${'3'.green}. Listar Tareas Completadas
+                         ${'4'.green}. Listar Tareas Pendientes
+                         ${'5'.green}. Completar tareas
+                         ${'6'.green}. Borrar una Tarea
+                         ${'0'.green}. Salir`)
+
+
+            const readline = require('readline').createInterface({
+                input: process.stdin,
+                output: process.stdout,
+            });
+            readline.question(`Seleccione una opción \n` ,  (opt)=> {
+                readline.close();
+                resolve(opt)
+            })
+        })
+
+
+}
+
+
+const pausa = () => 
+{
+    return new Promise(resolve => 
+        {
+        const readline = require('readline').createInterface({
+            input: process.stdin,
+            output: process.stdout,
+        });
+        readline.question(`Presione ${'Enter'.green} para continuar \n` ,  (opt)=> {
+            readline.close();
+            resolve();
+        })
+    })
+}
+
+
+module.exports =  {
+    mostrarMenu,
+    pausa
+}
