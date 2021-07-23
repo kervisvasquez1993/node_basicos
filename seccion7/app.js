372c9ef45817ca4d7b0cb141fc91b37d2e0e4723
kervis vasquez <kervisvasquez24@gmail.com>
2021-07-23 16:49:26 -0400

    agregamos archivo estaticos en public

--- seccion7/app.js
+++ seccion7/app.js
@@ -1,23 +1,36 @@
 const express = require('express')
 const app = express()
-const port = 8003
+const port = 8004
 
 //servir contenido estatico 
 
 app.use(express.static('public')) 
  
 app.get('/',  (req, res) =>  {
-  res.send('Hello World')
+  res.sendFile(__dirname + '/public/index.html')
 })
 
-app.get('/hola',  (req, res) =>  {
+app.get('/elements',  (req, res) =>  {
+  res.sendFile(__dirname + '/public/elements.html')
+})
+
+app.get('/generic',  (req, res) =>  {
+  res.sendFile(__dirname + '/public/generic.html')
+})
+
+/* 
+app.get('*',  (req, res) =>  {
+  res.sendFile(__dirname +'/public/404.html');
+}) */
+
+
+/* app.get('/hola',  (req, res) =>  {
     res.send('hola mundo desde express')
   })
+ */
 
-  app.get('*',  (req, res) =>  {
-    res.sendFile(__dirname +'/public/404.html');
-  })
- 
+
+  
 app.listen(port, () => {
   console.log(`corriendo por el puerto http://localhost:${port}`)
 })
