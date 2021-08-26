06f00612e5c39139845cfc00a409a7265654daf6
kervis vasquez <kervisvasquez24@gmail.com>
2021-08-26 11:16:40 -0400

    agregamos modelo para cliente

--- api_juan/app.js
+++ api_juan/app.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const router = require("./router");
 const port   = 3000;
+const mongoose = require('mongoose');
+/* conectar mongo */
+mongoose.Promise = global.Promise;
+mongoose.conectar = ('mongod://localhost:27017/restapi', {
+    userNewUrlParser : true,
+});
 
 //crear servidor
 const app = express();
