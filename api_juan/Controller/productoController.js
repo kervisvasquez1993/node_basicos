c87241c93e098c55f57cc30d17a18c4478fc3560
kervis vasquez <kervisvasquez24@gmail.com>
2021-08-27 14:29:28 -0400

    agregamos cambios en nuestro proyecto para producto

--- api_juan/Controller/productoController.js
+++ api_juan/Controller/productoController.js
@@ -1,67 +1,83 @@
-const multer = require('multer');
-const shortid = require('shortid');
+const multer = require("multer");
+const shortid = require("shortid");
 const Productos = require("../Models/Productos");
 
-
 const configuracionMulter = {
-    
-    storege : fileStorage = multer.diskStorage({
-        destination: (req, file,cb) => 
-        {
-            cb(null, __dirname+'../..uploads');
+    storege: (fileStorage = multer.diskStorage({
+        destination: (req, file, cb) => {
+            cb(null, __dirname + "../..uploads");
         },
-        filename: (req,file,cb) => 
-        {
-            const extension = file.mimetype.split('/')[1];
+        filename: (req, file, cb) => {
+            const extension = file.mimetype.split("/")[1];
             cb(null, `${shortid.generate()}.${extension}`);
+        },
+    })),
+    fileFilter(req, file, cb) {
+        if (file.mimetype === "image/jpeg" || file.mimetype === "image/png") {
+            cb(null, true);
+        } else {
+            cb(new Error("Formato no valido"));
         }
-    }),
-    fileFilter(req,file,cb)
-    {
-        if(file.mimetype === 'image/jpeg' || file.mimetype === 'image/png')
-        {
-            cb(null,true);
-        }
-        else {
-            cb(new Error('Formato no valido'))
-        }
-    }
-    
-}
+    },
+};
 
-
-const upload = multer(configuracionMulter).single('imagen');
+const upload = multer(configuracionMulter).single("imagen");
 
 // sube un archivo
 
-exports.subirArchivo = (req,res,next) => 
-{
-    upload(req,res, function(error)
-    {
-        if(error)
-        {
-             res.json({mensjae : error})
+exports.subirArchivo = (req, res, next) => {
+    upload(req, res, function (error) {
+        if (error) {
+            res.json({ mensjae: error });
         }
-        
-        return next();
-    })
-}
 
+        return next();
+    });
+};
 
 exports.nuevoProducto = async (req, res, next) => {
     const producto = new Productos(req.body);
-    try
-    {
-        if(req.file.filename)
-        {
-            producto.imagen = req.file.filename
+    try {
+        if (req.file.filename) {
+            producto.imagen = req.file.filename;
         }
         await producto.save();
-        return  res.json({data : 'Se agrego un nuevo producto'})
-    }
-    catch (err)
-    {
-        console.log(err)
+        return res.json({ data: "Se agrego un nuevo producto" });
+    } catch (err) {
+        console.log(err);
         return next();
+    }
+};
+
+exports.indexProducto = async (req, res, next) => {
+    try {
+        //    obtener todos los productos //
+        const productos = await Productos.find({});
+        return res.json(productos);
+    } catch (err) {
+        console.log({ error: err });
+        return next();
+    }
+};
+
+exports.showProducto = async (req, res, next) => {
+    const producto = await Productos.findById(req.params.idProducto);
+    if (!producto) {
+        res.json({ data: "Este producto no existe" });
+        return next();
+    }
+    res.json(producto);
+};
+// actualiza un producto via id
+exports.updateProducto = async (req, res, next) => {
+    try {
+        let producto = await Productos.findOneAndUpdate({
+            _id: req.params.idProducto },
+            req.body, {
+                new : true,
+            });
+        return res.json(producto);
+    } catch (error) {
+        console.log(error);
     }
 };
--- api_juan/Router/index.js
+++ api_juan/Router/index.js
@@ -1,25 +1,38 @@
-const express = require('express');
-const router  = express.Router();
-const clienteController = require('../Controller/clienteController');
-const productoController = require('../Controller/productoController');
+const express = require("express");
+const router = express.Router();
+const clienteController = require("../Controller/clienteController");
+const productoController = require("../Controller/productoController");
 
-module.exports = function(){
+module.exports = function () {
     // traer los clientes
-    router.get('/clientes', clienteController.mostrarCliente)
+    router.get("/clientes", clienteController.mostrarCliente);
     // agregar nuevos clientes via post
-    router.post('/clientes', clienteController.nuevoCliente);
+    router.post("/clientes", clienteController.nuevoCliente);
     // get show client
-    router.get('/clientes/:idCliente', clienteController.showCliente);
+    router.get("/clientes/:idCliente", clienteController.showCliente);
     // update
-    router.put('/clientes/:idCliente', clienteController.updateCliente);
+    router.put("/clientes/:idCliente", clienteController.updateCliente);
     // eliminar registro
-    router.delete('/clientes/:idCliente', clienteController.removeCliente);
+    router.delete("/clientes/:idCliente", clienteController.removeCliente);
 
+    // productos
 
+    router.post(
+        "/productos",
+        productoController.subirArchivo,
+        productoController.nuevoProducto
+    );
+    // mostart productos
+    router.get("/productos", productoController.indexProducto);
+    // tarer un producto en especifico
+    router.get("/productos/:idProducto", productoController.showProducto);
+    // update productos
 
-    // productos 
+    router.put(
+        "/productos/:idProducto",
+        productoController.subirArchivo,
+        productoController.updateProducto
+    );
 
-    router.post('/productos', productoController.subirArchivo, productoController.nuevoProducto);
     return router;
-}
-
+};
