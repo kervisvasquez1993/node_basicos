2a25a416c289667818c0b87554b2b0000d5d2073
kervis vasquez <kervisvasquez24@gmail.com>
2021-09-03 10:27:24 -0400

    agregamos el get de nuestros usuarios con argumentos asignado

--- repaso_api/Controller/UserController.js
+++ repaso_api/Controller/UserController.js
@@ -2,60 +2,54 @@ const { response, request } = require("express");
 const bcryptjs = require("bcryptjs");
 const User = require("../Models/User");
 
-const userGet = (req = request, res = response) => {
+const userGet = async (req = request, res = response) => {
     const query = req.query; /* se puede desectructurar */
-    res.json({data : query});
-
+    const { limit = 2, desde = 0 } = req.query;
+    const users = await User.find().skip(Number(desde)).limit(Number(limit));
+    res.json({ data: users });
 };
 
 const userPost = async (req = request, res = response, next) => {
-   
-    const {name, email, password, rol } = req.body;
-    try
-    {
-    const usuario = new User({name,email,password,rol});
+    const { name, email, password, rol } = req.body;
+    try {
+        const usuario = new User({ name, email, password, rol });
 
-    // verificar que el correo existe  
-    const correoExist = await User.findOne({email})
-    if(correoExist)
-    {
-        /* return res.status(400).json({
+        // verificar que el correo existe
+        const correoExist = await User.findOne({ email });
+        if (correoExist) {
+            /* return res.status(400).json({
             data : "correo ya registrado"
         }); */
-    }
-    
-    // encryptar
-    const salt = bcryptjs.genSaltSync();
-    usuario.password = bcryptjs.hashSync(password, salt);
+        }
 
-    await usuario.save();
-    return res.json({
-        data : usuario
-    });
-    }
-    catch(error)
-    {
-        throw(error);
+        // encryptar
+        const salt = bcryptjs.genSaltSync();
+        usuario.password = bcryptjs.hashSync(password, salt);
+
+        await usuario.save();
+        return res.json({
+            data: usuario,
+        });
+    } catch (error) {
+        throw error;
         return next();
     }
 };
 
 const userPut = async (req, res = response) => {
     const id = req.params.idUser;
-    const {_id, password, google, email,...resto} = req.body;
+    const { _id, password, google, email, ...resto } = req.body;
     // TODO: VALIDADR CONTRA BASE DE DATOS
-    if(password){
+    if (password) {
         const salt = bcryptjs.genSaltSync();
         resto.password = bcryptjs.hashSync(password, salt);
     }
 
-
-    const usuario = await User.findByIdAndUpdate( id, resto)
-    
+    const usuario = await User.findByIdAndUpdate(id, resto);
 
     return res.json({
-        data : usuario
-    })
+        data: usuario,
+    });
 };
 
 const userDelete = (req, res = response) => {
