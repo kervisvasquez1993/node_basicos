b7bab8b9c8abfb199042f915fbfe5509dfadaea7
kervis vasquez <kervisvasquez24@gmail.com>
2021-09-09 14:09:10 -0400

    agregamos index en middleware para organizar todos los middleware en un solo archivo

---
+++ repaso_api/middlewares/index.js
@@ -0,0 +1,11 @@
+const  validarCampos  = require("../middlewares/validaCampo");
+const  validarJWT  = require("../middlewares/validarJWT");
+const  validaRoles = require("../middlewares/validar-admin");
+
+
+
+module.exports = {
+    ...validarCampos,
+    ...validarJWT,
+    ...validaRoles,
+}
