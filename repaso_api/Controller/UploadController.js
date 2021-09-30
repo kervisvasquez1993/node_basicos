d08ad2c51d52638e33f4b9918d7d3fb84471fdc0
kervis vasquez <kervisvasquez24@gmail.com>
2021-09-30 10:14:55 -0400

    cambiar el nombre del archivo y validar la extension

--- repaso_api/Controller/UploadController.js
+++ repaso_api/Controller/UploadController.js
@@ -1,9 +1,8 @@
 const path = require("path");
+const { v4: uuidv4 } = require("uuid");
 const { response, request } = require("express");
 
-
 const UploadFile = async (req, res = response) => {
-
     let uploadPath;
 
     if (
@@ -16,29 +15,26 @@ const UploadFile = async (req, res = response) => {
     }
 
     const { archivos } = req.files;
-    const nombreCortado  = archivos.name.split('.')
+    const nombreCortado = archivos.name.split(".");
     const extension = nombreCortado[nombreCortado.length - 1];
     // validar la extension en el controlador
-    const extensionValida = ["png", "jpg", "gif"]
+    const extensionValida = ["png", "jpg", "gif"];
 
-    if(!extensionValida.includes(extension)) {
-        return res.status(400).json({data : `La extension permitidas son , ${extensionValida}`});
+    if (!extensionValida.includes(extension)) {
+        return res
+            .status(400)
+            .json({ data: `La extension permitidas son , ${extensionValida}` });
     }
 
-    
+    const nameTemp = uuidv4() + "." + extension;
 
-    return res.json(extension)
-
-    // uploadPath = path.join(__dirname,"../uploads/" + archivos.name);
-    
-
-    // archivos.mv(uploadPath,  (err) => {
-    //     if (err) {
-    //         return res.status(500).json(err);
-    //     }
-
-    //     res.json("File uploaded to " + uploadPath);
-    // });
+    uploadPath = path.join(__dirname, "../uploads/" + nameTemp);
+    archivos.mv(uploadPath, (err) => {
+        if (err) {
+            return res.status(500).json(err);
+        }
+        res.json("File uploaded to " + uploadPath);
+    });
 };
 
 module.exports = {
